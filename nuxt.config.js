fbfdc6fc283b9815e68df3f3fa1bf0ebaad28f8a
rcanai <canai3jp@gmail.com>
2019-12-25 17:31:50 +0900

    sttaic/にpublicディレクトリを作成

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'simple-sls-nuxt' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/static/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/public/favicon.ico' }
     ]
   },
   /*
